8643fac8009b76206d64025f212a8e65006b7e9d
jmptc <carner1024@gmail.com>
2022-05-21 00:50:47 -0500

    nato01

---
+++ nato/nato01.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "unicode"
+)
+
+func main() {
+    nato := []string { "Alpha", "Bravo", "Charile", "Delta", "Echo", "Foxtrot",
+        "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November",
+        "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor",
+        "Whiskey", "Xray", "Yankee", "Zulu" }
+
+    fmt.Print("Enter a word or a phrase: ")
+    scanner := bufio.NewScanner(os.Stdin)
+    scanner.Scan()
+    text := scanner.Text()
+
+    for _, c := range text {
+        c = unicode.ToLower(c)
+        idx := int(c - 'a')
+        if idx < len(nato) && idx >= 0 {
+            fmt.Println(nato[idx])
+        } else {
+            fmt.Println("")
+        }
+    }
+}
